20c5e2b01533ea1c68f94376a36e90c555eecd47
gleb5783 <gleb5783@mail.ru>
2022-01-17 11:41:22 +0300

    last

---
+++ source/js/slick.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider__list').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: true,
+        centerMode: true,
+        initialSlide: 0,
+        infinite: true
+    });
+  });
